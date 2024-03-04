74093d428288bd37e9adba76cf004c2be4d91101
BHUTUU <raj259942@gmail.com>
2024-03-05 00:14:42 +0530

    little update in manager file

--- cloudflareManager.py
+++ cloudflareManager.py
@@ -1,14 +1,19 @@
-import os, sys, subprocess, time, re
+import os, sys, subprocess, time, re, threading
 class CloudflareManager:
+    __cloudfaredServerAndPid = {}
     __cloudfaredPid = []
     __prootPids = []
+    logPathDir=""
     if os.name == 'nt':
         binPath = "C:\\Program Files\\cloudflare\\bin"
+        logPathDir = "C:\\Program Files\\cloudflare\\log"
     if os.name == 'posix':
         if "/data/data/com.termux/files" in os.getcwd():
             binPath = "/data/data/com.termux/files/usr/bin"
+            logPathDir = "/data/data/com.termux/files/home/cloudflare/log"
         else:
             binPath = "/usr/bin"
+            logPathDir = "/home/cloudflare/log"
     @classmethod
     def __runServer(cls, lhost: str, lport: int, logfilePath: str):
         if os.name == 'nt':
@@ -60,5 +65,28 @@ class CloudflareManager:
         if len(cls.__cloudfaredPid) > currentPidListLen:
             return cls.__cloudfaredPid[-1]
         return validPid
+    @staticmethod
+    def startCloudflareServer(lhost: str, lport: int, logfilePath: str=os.path.join(logPathDir, 'log.txt')):
+        updateCurrentPids = CloudflareManager.__getPidOfServer()
+        __key = f"{lhost}:{lport}"
+        if CloudflareManager.__cloudfaredServerAndPid.get(__key) is not None:
+            if str(CloudflareManager.__cloudfaredServerAndPid.get(__key)) == str(updateCurrentPids):
+                return [False, "This server is already in use! Try again with different lhost and lport values!!"]
+        threading.Thread(target=CloudflareManager.__runServer(lhost, lport, logfilePath)).start()
+        time.sleep(1)
+        currentPid = CloudflareManager.__getPidOfServer()
+        CloudflareManager.__cloudfaredServerAndPid[__key] = currentPid
+        link=None
+        while True:
+            with open(logfilePath, "r") as logfile:
+                content = logfile.readlines()
+                if len(content) > 0:
+                    for line in content:
+                        link = re.findall(r'https://[-0-9a-z]*\.trycloudflare.com', line)
+                        if link:
+                            break
+                    break
+        return link
+
 
             
